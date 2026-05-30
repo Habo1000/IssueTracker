50532819834602da2a3e52ec1e84bb150ab03623
boudelal <haroune.boudelal@etu.unistra.fr>
2026-05-30 17:14:16 +0200

    style for active link

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import { GoAlertFill } from "react-icons/go";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const NavBar = () => {
   const links = [
@@ -14,15 +18,27 @@ const NavBar = () => {
     },
   ];
 
+  const currentpath = usePathname();
+
   return (
-    <nav className="flex items-center justify-around border-b px-5 py-3">
-      <Link href="/">
-        <GoAlertFill />
+    <nav className="flex items-center justify-around border-b px-5 py-3 h-16">
+      <Link href="/" className="hover:text-orange-500">
+        <GoAlertFill size={32} />
       </Link>
       <ul className="flex list-none gap-8 ">
         {links.map(({ path, name }) => (
           <li key={path}>
-            <Link href={path}>{name}</Link>
+            <Link
+              href={path}
+              className={classnames({
+                "text-[rgb(var(--foreground-rgb))]": currentpath === path,
+                "text-zinc-400": currentpath !== path,
+                "hover:text-orange-500": true,
+                "transition-colors": true,
+              })}
+            >
+              {name}
+            </Link>
           </li>
         ))}
       </ul>
