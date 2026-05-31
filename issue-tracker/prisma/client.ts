b6597fe827633dd48375738ee563e95927eff685
boudelal <haroune.boudelal@etu.unistra.fr>
2026-05-31 18:57:06 +0200

    API for posting issues

---
+++ issue-tracker/prisma/client.ts
@@ -0,0 +1,26 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "./generated/prisma/client";
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  port: 3306,
+  connectionLimit: 5,
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "issue-tracker",
+});
+
+const prismaSingleton = () =>
+  new PrismaClient({
+    adapter,
+  });
+
+type PrismaSingleton = ReturnType<typeof prismaSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
