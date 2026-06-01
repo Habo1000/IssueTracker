67c891c648ba940bc151297ea1523f3250affd08
boudelal <haroune.boudelal@etu.unistra.fr>
2026-06-01 19:37:55 +0200

    Radix UI Theme

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -3,7 +3,7 @@ import { TextField, TextArea, Button } from "@radix-ui/themes";
 
 const NewIssuePage = () => {
   return (
-    <div className="flex flex-col gap-4 max-w-xl p-4 ">
+    <div className="flex flex-col gap-4 max-w-xl py-8 m-auto">
       <TextField.Root placeholder="Title" />
       <TextArea placeholder="Description of the issue" />
       <Button>Submit New Issue</Button>
--- issue-tracker/app/layout.tsx
+++ issue-tracker/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,9 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.variable}>
         <main>
-          <Theme>
+          <Theme accentColor="orange" radius="large">
             <NavBar />
             {children}
           </Theme>
